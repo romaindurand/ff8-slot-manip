f5e696940e2c14c730277d21861b8fb8f5ba06bc
Kaivela <kaivel100@gmail.com>
2022-07-25 01:25:39 +0200

    redefined FAQ

--- src/components/FAQ.svelte
+++ src/components/FAQ.svelte
@@ -21,17 +21,19 @@
 
 {#if category === 'NoJunction'}
 <ul class="w-full max-w-lg">
-  <li>Currently Working only on lvl 8, with max Hp at 482, targeted spell is The End.</li>
+  <li class="red">Coming Soon TM.</li>
+  <!-- <li>Currently Working only on lvl 8, with max Hp at 482, targeted spell is The End.</li>
   <li>During a freeze atb moment like a Guardian Force summon or a long limit break animation like invicible moon.</li>
-  <li>The range of HP that works is equal or under 54 hp otherwise you can't get THE END, and the proper hp setup is equal or under 34 hp because it unlocks the spell instant on 2nd do over.</li>
+  <li>The range of HP that works is equal or under 54 hp otherwise you can't get THE END, and the proper hp setup is equal or under 34 hp because it unlocks the spell instant on 2nd do over.</li> -->
 </ul>
 {/if}
 
 {#if category === 'NoJunction/NoLevel'}
 <ul class="w-full max-w-lg">
-  <li>Currently Working only on lvl 8, with max Hp at 482, targeted spell is The End.</li>
+  <li class="red">Coming Soon TM.</li>
+  <!-- <li>Currently Working only on lvl 8, with max Hp at 482, targeted spell is The End.</li>
   <li>During a freeze atb moment like a Guardian Force summon or a long limit break animation like invicible moon.</li>
-  <li>The range of HP that works is equal or under 54 hp otherwise you can't get THE END, and the proper hp setup is equal or under 34 hp because it unlocks the spell instant on 2nd do over.</li>
+  <li>The range of HP that works is equal or under 54 hp otherwise you can't get THE END, and the proper hp setup is equal or under 34 hp because it unlocks the spell instant on 2nd do over.</li> -->
 </ul>
 {/if}
 <style> 
